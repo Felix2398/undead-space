bec854d2a83fba25e32e2d30cd6f80da0794932f
Allan Penkhasov <allan.penkhasov@brixware.com>
2024-03-13 12:51:53 +0100

    Added all weapon buffs and fixed sound bug

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -25,13 +25,12 @@ public class PowerUp : MonoBehaviour
 
         if(collider.gameObject.tag == "Player") {
             
-            // gameObject.GetComponent<MeshRenderer>().enabled = false;
-            // gameObject.GetComponent<BoxCollider>().enabled = false;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
 
             powerupEffect.Apply(collider.gameObject);
-            // audioSource.Play();
-            // StartCoroutine(DestroyAfterSound());
-            Destroy(gameObject);
+            audioSource.Play();
+            StartCoroutine(DestroyAfterSound());
         }
     }
 
