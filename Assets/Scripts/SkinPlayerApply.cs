946b975ec6a8e56941d4c810f0e564ec8d84f4eb
Osman Yozgat <osmanyozgat@swla004.WLAN.Uni-Marburg.DE>
2024-03-14 12:52:34 +0100

    add SkinSelector

---
+++ Assets/Scripts/SkinPlayerApply.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SkinPlayerApply : MonoBehaviour
+{
+    void Start()
+    {
+        // Lese den ausgewählten Skin-Index und das Material aus den PlayerPrefs
+        int selectedSkinIndex = PlayerPrefs.GetInt("SelectedSkinIndex", 0);
+        string selectedSkinMaterialName = PlayerPrefs.GetString("SelectedSkinMaterial", "");
+
+        // Wende das Material auf den Spieler an
+        Renderer playerRenderer = GetComponent<Renderer>();
+        Material[] playerMaterials = playerRenderer.materials;
+        playerMaterials[0] = FindMaterialByName(selectedSkinMaterialName);
+        playerRenderer.materials = playerMaterials;
+    }
+
+    Material FindMaterialByName(string materialName)
+    {
+        // Finde das Material im Assets-Ordner nach dem Namen
+        return Resources.Load<Material>(materialName);
+    }
+}
