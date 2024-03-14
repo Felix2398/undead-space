52a4d514653d9d47f025b42350ba37b312bd26f9
Allan Penkhasov <allan.penkhasov@brixware.com>
2024-03-14 20:12:06 +0100

    filled license information

--- Assets/Scripts/UIElements/LinkText.cs
+++ Assets/Scripts/UIElements/LinkText.cs
@@ -16,7 +16,19 @@ public class LinkText : MonoBehaviour, IPointerClickHandler
 
     linkText.text = 
         linkColor + "<link=https://www.flaticon.com/de/kostenlose-icons/muskel>Muskel Icons erstellt von Vitaly Gorbachev - Flaticon</link><br>" + endColor +
-        linkColor + "<link=https://www.flaticon.com/de/kostenlose-icons/donner>Donner Icons erstellt von Smashicons - Flaticon</link><br>" + endColor;
+        linkColor + "<link=https://www.flaticon.com/de/kostenlose-icons/donner>Donner Icons erstellt von Smashicons - Flaticon</link><br>" + endColor + 
+        linkColor + "<link=https://kenney.nl/assets/blaster-kit>Kenny Blaster Kit</link><br>" + endColor +
+        linkColor + "<link=https://kenney.nl/assets/animated-characters-1>Kenny Animated Characters 1</link><br>" + endColor +
+        linkColor + "<link=https://kenney.nl/assets/animated-characters-2>Kenny Animated Characters 2</link><br>" + endColor +
+        linkColor + "<link=https://www.mixamo.com>Dying, Rifle Aim Idle, Rifle Idle, Rifle Run, Silly Dancing, Two Cycle Sprint, Zombie Dying, Zombie Idle, Zombie Punch, Zombie Run, Zombie Walk</link><br>" + endColor +
+        linkColor + "<link=https://assetstore.unity.com/packages/vfx/particles/particle-pack-127325>Unity Partical Pack</link><br>" + endColor + 
+        linkColor + "<link=https://assetstore.unity.com/packages/3d/props/ten-power-ups-217666>Unity Power Ups</link><br>" + endColor + 
+        linkColor + "<link=https://pixabay.com/music/>In-Game music from Pixabay</link><br>" + endColor +
+        linkColor + "<link=https://kenney.nl/assets/interface-sounds>UI and In-Game sounds</link><br>" + endColor + 
+        linkColor + "<link=https://freesound.org/people/pgi/sounds/212607/>Machine Gun 002 - single shot</link><br>" + endColor +
+        linkColor + "<link=https://freesound.org/people/PNMCarrieRailfan/sounds/683311/>Pistol Gun Shot Mix</link><br>" + endColor +
+        linkColor + "<link=https://freesound.org/people/Carlfnf/sounds/677839/>Sniper Shoot</link><br>" + endColor +
+        linkColor + "<link=https://freesound.org/people/unfa/sounds/609587/>Grenade Explosion SFX (medium-sized, meaty, realistic)</link><br>" + endColor;
     }
 
     public void OnPointerClick(PointerEventData eventData) {
